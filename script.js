be73fc8f1cce67170bc16d37b8e778528b4214cb
NikCook123 <ncook1@ocdsb.ca>
2024-11-15 10:49:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,54 @@
-    const startStopButton = document.getElementById('startStopButton');
+        const startStopButton = document.getElementById('startStopButton');
         const resultDiv = document.getElementById('result');
-        const stupidthing = document.getElementById('dumbshit');
-        stupidthing.onclick=textToSpeech;
-        const textStorage = document.getElementById('textInput');
         const SpeechRecognition = window.SpeechRecongition || window.webkitSpeechRecognition;
         const recognition = new SpeechRecognition();
         recognition.interimResults = true;
         recognition.continuous = true;
         let listening = false;
         let transcript = ' ';
-        var wordArray = [];
-        var remain ='';
-        var record = true;
         var result ="";
         var command = false;
         var word = transcript.substring(transcript.lastIndexOf(" "),transcript.length);
+
+        var commandPath = "";
+        var commandWordBank_First = [
+            "start", 
+            "stop",
+            "change",
+            "restore",
+            "list",
+        ];
+
+        var commandWordBank_Second = [
+            "read", 
+            "write",
+            "text",
+        ]
+
+        var commandWordBank_third = [
+            "size",
+            "color",
+            "font",
+        ]
+
         recognition.onresult = (event) => {
             transcript = ' ';
             word = transcript.substring(transcript.lastIndexOf(" "),transcript.length-1);
+
             for (let i = 0; i < event.results.length; i++) {
                 transcript += event.results[i][0].transcript;
             }
-            resultDiv.innerHTML = transcript;
-            remain = transcript;
             
-            for(i=0;i<wordArray.length;i++){
-                wordArray[i] = remain.substring(0,remain.indexOf(" "));
-                remain = remain.substring(wordArray[i].length,remain.length);
-            }
+            resultDiv.innerHTML = transcript;
+
             if(transcript.substring(transcript.lastIndexOf(" "),transcript.length)==" command"){
                 command=true;
             }
+
             if(command){
                 commandWords(transcript);
             }
+
             if(transcript.substring(transcript.lastIndexOf(" "),transcript.length)==" over"){
                 command=false;
             }
@@ -61,30 +76,57 @@
             }
         })
 
-        function textToSpeech(){
-            window.speechSynthesis.cancel();
-            let text = textStorage.value;
-            let speech = new SpeechSynthesisUtterance();
-            speech.text = text;
-            speech.volume = 1;
-            speech.rate = 1;
-            speech.pitch = 1;
-            window.speechSynthesis.speak(speech);
-        }
+        
         function commandWords(word){
             var shortWord = word.substring(word.lastIndexOf("command"),word.length);
-            console.log(shortWord);
-            switch (word){
-                case ' stop':
-                    record=false;
-                    break;
-                case ' start':
-                    record = true;
-                    break;
-                case ' read':
-                    textToSpeech();
-                    break;
-                default: 
-                    break;
+            //console.log(commandWordBank[0]);
+            for(i=0;i<commandWordBank_First.length;i++){
+                if(searchSequence(shortWord, "command "+commandWordBank_First[i])){
+                    shortWord = shortWord.substring(shortWord.lastIndexOf(commandWordBank_First[i]),shortWord.length);
+                    switch(shortWord){
+                        case "start read":
+                            console.log("start read")
+                            break;
+                        case "start write":
+                            console.log("start write")
+                            break;
+                        case "stop read":
+                            console.log("stop read")
+                            break;
+                        case "stop write":
+                            console.log("stop write")
+                            break;
+                        case "change text font":
+                            console.log("change text font")
+                            break;
+                        case "change text size":
+                            console.log("change text size")
+                            break;
+                        case "change text color":
+                            console.log("change text color")
+                            break;
+                        case "restore text font":
+                            console.log("restore text font")
+                            break;
+                        case "restore text size":
+                            console.log("restore text size")
+                            break;
+                        case "restore text color":
+                            console.log("restore text color")
+                            break;
+                        case "list":
+                            console.log("list")
+                            break;
+                        default:
+                            break;
+                    }
+                }
             }
+            if(searchSequence(shortWord, "over")){
+                console.log(shortWord);
+            }
+        }
+
+        function searchSequence(shortWord, sequence) {
+            return shortWord.includes(sequence);
         }
