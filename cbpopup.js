8e971a3ac5913c9bf4182705d169c29235b638f0
EatMyGames <nicholas.g.worthing@gmail.com>
2024-11-21 11:19:00 -0500

    Co-authored-by: WywySenarios <WywySenarios@users.noreply.github.com> Co-authored-by: davidssimon <davidssimon@users.noreply.github.com> Co-authored-by: shiqi5858 <shiqi5858@users.noreply.github.com> Co-authored-by: lintrii <lintrii@users.noreply.github.com> Co-authored-by: amrssd <amrssd@users.noreply.github.com>

--- cbpopup.js
+++ cbpopup.js
@@ -1,4 +1,4 @@
-document.getElementById("contrast").addEventListener("click", function() {
+document.getElementById("contrast-toggle").addEventListener("click", function() {
     // Open the second popup
     let url = chrome.runtime.getURL("cbpopup.html"); // Adjust if needed
     window.open(url, "_blank", "width=400,height=300");
